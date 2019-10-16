3157fafa130959ea8333e90b6ab7ab4ca8222d44
Urbancova <urbancova.lucia@gmail.com>
2019-10-16 10:48:00 +0200

    advanced added

---
+++ Advanced/script.js
@@ -0,0 +1,11 @@
+//ctrl+shift+t to compile//
+//Create a Parent Class called Vehicles//
+//Consider the inheritance concept//
+var Vehicles = /** @class */ (function () {
+    function Vehicles() {
+    }
+    return Vehicles;
+}());
+//Based on the personal vehicle performance model, kilometers left, number of seats, 
+//fuel type and year of production calculate the price once the user clicks on the button 
+//"show price" *use your dummy custom formula)
---
+++ Advanced/script.ts
@@ -0,0 +1,18 @@
+//ctrl+shift+t to compile//
+
+//Create a Parent Class called Vehicles//
+//Consider the inheritance concept//
+
+class Vehicles {
+  model : string;
+  kilometers : number;
+  seats : number;
+  fuel : string;
+  productionYR: number;
+}
+
+
+//Based on the personal vehicle performance model, kilometers left, number of seats, 
+  //fuel type and year of production calculate the price once the user clicks on the button 
+    //"show price" *use your dummy custom formula)
+
