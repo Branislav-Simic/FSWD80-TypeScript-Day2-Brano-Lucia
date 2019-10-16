924255c95af06707cbe31b576029ce7c69d8b03d
Branislav Simic <branislavsmc@outlook.de>
2019-10-16 10:28:49 +0200

    basic 2 done

--- basic/basic.js
+++ basic/basic.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var person = /** @class */ (function () {
     function person(firstName, lastName, age, jobTitle) {
         this.firstName = "";
@@ -16,3 +29,20 @@ var person = /** @class */ (function () {
 }());
 var fullName = new person("Brano", "Simic", "20", "student");
 document.getElementById('doc').innerHTML = fullName.out();
+// basic 2 start
+var job1 = /** @class */ (function (_super) {
+    __extends(job1, _super);
+    function job1(firstName, lastName, age, jobTitle, salary, jobLocation) {
+        var _this = _super.call(this, firstName, lastName, age, jobTitle) || this;
+        _this.salary = salary;
+        _this.jobLocation = jobLocation;
+        return _this;
+    }
+    job1.prototype.some = function () {
+        return "and i get " + this.salary + " \u20AC every month, and I work in Vienna.";
+    };
+    return job1;
+}(person));
+var job = new job1("Brano", "Simic", "20", "student", "800", "Vienna");
+document.getElementById('doc').innerHTML += job.some();
+// += use by add sentence to other previous sentence!!!
--- basic/basic.ts
+++ basic/basic.ts
@@ -20,4 +20,26 @@ var fullName = new person("Brano","Simic","20","student");
 document.getElementById('doc').innerHTML= fullName.out();
 
 
+// basic 2 start
+
+class job1 extends person {
+	salary;
+	jobLocation;
+
+	constructor (firstName,lastName,age,jobTitle,salary,jobLocation){
+		super(firstName,lastName,age,jobTitle);
+		this.salary=salary;
+		this.jobLocation=jobLocation;
+	}
+	some(){
+		return `and i get ${this.salary} € every month, and I work in Vienna.`;
+	}
+}
+
+var job = new job1("Brano","Simic","20","student","800","Vienna");
+document.getElementById('doc').innerHTML += job.some();  
+// += use by add sentence to other previous sentence!!!
+
+
+
 
